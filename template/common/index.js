ed2b344992080dfb781827e5d2b848f75718580a
rgmxg <rgxmg92@163.com>
2020-08-17 14:46:54 +0800

    feat(interceptor): 新增拦截器template生成

--- template/common/index.js
+++ template/common/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
-import { useRequestInterceptor, useResponseInterceptor } from './util';
-import { createRequestInterceptorMiddleware, createResponseInterceptorMiddleware } from './interceptor';
+<% import { useRequestInterceptor, useResponseInterceptor } from './util'; %>
+<% import { createRequestInterceptorMiddleware, createResponseInterceptorMiddleware } from './interceptor'; %>
 <% _.forEach(config.projects, function(project){ %>import * as {{$$.convertUrl(project.name)}} from './{{project.name}}';
 <% }) %>
 
 // register request interceptor
-useRequestInterceptor(req => createRequestInterceptorMiddleware(req));
+<% useRequestInterceptor(req => createRequestInterceptorMiddleware(req)); %>
 
 // register response interceptor
-useResponseInterceptor(res => createResponseInterceptorMiddleware(res));
+<% useResponseInterceptor(res => createResponseInterceptorMiddleware(res)); %>
 
-export {<% _.forEach(config.projects, function(project, i){ %>
+exports {<% _.forEach(config.projects, function(project, i){ %>
   {{$$.convertUrl(project.name)}}<% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
 };
