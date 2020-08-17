9d9556b9eac4ae35d859f531a166df025d7b6395
rgmxg <rgxmg92@163.com>
2020-08-17 14:47:46 +0800

    feat(interceptor): 新增拦截器template生成

--- template/common/index.js
+++ template/common/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
-<% import { useRequestInterceptor, useResponseInterceptor } from './util'; %>
-<% import { createRequestInterceptorMiddleware, createResponseInterceptorMiddleware } from './interceptor'; %>
+import { useRequestInterceptor, useResponseInterceptor } from './util';
+import { createRequestInterceptorMiddleware, createResponseInterceptorMiddleware } from './interceptor';
 <% _.forEach(config.projects, function(project){ %>import * as {{$$.convertUrl(project.name)}} from './{{project.name}}';
 <% }) %>
 
 // register request interceptor
-<% useRequestInterceptor(req => createRequestInterceptorMiddleware(req)); %>
+useRequestInterceptor(req => createRequestInterceptorMiddleware(req));
 
 // register response interceptor
-<% useResponseInterceptor(res => createResponseInterceptorMiddleware(res)); %>
+useResponseInterceptor(res => createResponseInterceptorMiddleware(res));
 
 exports {<% _.forEach(config.projects, function(project, i){ %>
   {{$$.convertUrl(project.name)}}<% if(config.projects.length - 1 !== i) { %>,<% } %><% }) %>
