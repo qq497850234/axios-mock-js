44a1179ae0ad4947781a3fe07a759b9f5a0e1eaa
rgmxg <rgxmg92@163.com>
2020-08-17 15:49:44 +0800

    feat(cover): cover增加project选择，拿出名称

--- template/cover/index.js
+++ template/cover/index.js
@@ -2,8 +2,8 @@
 import instance from './instance';
 import { convertRESTAPI } from '{{$$.relative("util")}}';
 
-<% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
-function {{$$.convertMethod(mock, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}(opts) {
+<% _.forEach(data.mocks, function(mock){ const project = _.find(config.projects, {id: data.project._id});%>/** {{mock.description}} */
+function {{$$.convertMethod(mock, project.urlPreprocessor)}}(opts) {
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}', opts)<%} else {%> '{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}'<% } %>,
