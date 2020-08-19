4102e6ec04fe26919a5b6a5cf07ea9419e780ce7
rgmxg <rgxmg92@163.com>
2020-08-19 15:45:06 +0800

    feat(cover): 增加axios请求方法的区分，如get及其他

--- template/cover/index.js
+++ template/cover/index.js
@@ -3,11 +3,14 @@ import instance from './instance';
 import { convertRESTAPI } from '{{$$.relative("util")}}';
 
 <% _.forEach(data.mocks, function(mock){ const project = _.find(config.projects, {id: data.project._id});%>/** {{mock.description}} */
-function {{$$.convertMethod(project.name, mock, project.urlPreprocessor)}}(opts) {
+function {{$$.convertMethod(project.name, mock, project.urlPreprocessor)}}(body = {}, opts = {}) {
   return instance({
     method: '{{mock.method}}',
     url: <% if($$.isREST(mock.url)) {%>convertRESTAPI('{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}', opts)<%} else {%> '{{$$.urlPreprocess(mock.url, _.find(config.projects, {id: data.project._id}).urlPreprocessor)}}'<% } %>,
-    opts: opts
+    opts: {
+      {{mock.method === 'get' ? 'params' : 'data'}}: body,
+      ...opts
+    }
   });
 }
 
