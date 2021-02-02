6adbfd32dffccead7f63569a148688ac49b2281d
rgmxg <rgxmg92@163.com>
2021-02-02 15:47:51 +0800

    feat(interceptor): 增加取消请求的hack处理

--- template/common/index.js
+++ template/common/index.js
@@ -12,7 +12,11 @@ useRequestInterceptor(req => createRequestInterceptorMiddleware(req));
 useResponseInterceptor(
   res => createResponseInterceptorMiddleware(res),
   error => {
-    Notification.error(error.message || '网络错误，请联系管理员');
+    // 在接口被取消时，也会触发，进行hack
+    if (!error.__CANCEL__) {
+      // 网络错误
+      Notification.error(error.message || '网络错误，请稍后重试或联系管理员');
+    }
     return { success: false };
   }
 );
